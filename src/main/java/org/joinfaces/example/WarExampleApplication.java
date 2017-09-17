4bde329346e166bc067facfbce0241b97b6d2e43
Lars Grefer <github@larsgrefer.de>
2017-09-17 14:36:08 +0200

    add Example code

---
+++ src/main/java/org/joinfaces/example/WarExampleApplication.java
@@ -0,0 +1,16 @@
+package org.joinfaces.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * @author Lars Grefer
+ */
+@SpringBootApplication
+public class WarExampleApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarExampleApplication.class, args);
+    }
+}
