ef7871e8bea5137ce932676054289c5f80e480f2
Marcelo Romulo Fernandes <persapiens@gmail.com>
2018-02-02 13:13:39 -0300

    joinfaces upgraded to 3.0.0.RC1

--- src/main/java/org/joinfaces/example/WarExampleApplication.java
+++ src/main/java/org/joinfaces/example/WarExampleApplication.java
@@ -2,7 +2,7 @@ package org.joinfaces.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * @author Lars Grefer
